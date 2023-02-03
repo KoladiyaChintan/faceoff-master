7000fa5875479b8ceac3bea8ac12e4e59aad7093
koladiyachintan <chintankoladiya4471@gmail.com>
2023-02-03 18:41:59 +0530

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT, () =>
+    console.log(`listen on server ${process.env.PORT}`),
+  );
+}
+bootstrap();
